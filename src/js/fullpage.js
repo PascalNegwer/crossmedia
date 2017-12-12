94998454c112a0bd58d26d8c0bb9a6f206a84ba7
Pascal Negwer <pascal@negwer.de>
2017-12-12 22:06:30 +0100

    [TASK] Removes unneccessary document.ready function from fullpage.js

--- src/js/fullpage.js
+++ src/js/fullpage.js
@@ -1,78 +1,76 @@
 var fullpage = function () {
-    $(document).ready(function() {
-        $('#fullpage').fullpage({
-            //Navigation
-            menu: '#main-nav',
-            //lockAnchors: false,
-            anchors:['home', 'facts', 'personalities', 'workExperience', 'stress', 'ending', 'imprint'],
-            //navigation: false,
-            //navigationPosition: 'none',
-            //navigationTooltips: [],
-            //showActiveTooltip: false,
-            //slidesNavigation: false,
-            //slidesNavPosition: 'bottom',
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: '#main-nav',
+        //lockAnchors: false,
+        anchors:['home', 'facts', 'personalities', 'workExperience', 'stress', 'ending', 'imprint'],
+        //navigation: false,
+        //navigationPosition: 'none',
+        //navigationTooltips: [],
+        //showActiveTooltip: false,
+        //slidesNavigation: false,
+        //slidesNavPosition: 'bottom',
 
-            //Scrolling
-            css3: true,
-            scrollingSpeed: 700,
-            autoScrolling: false,
-            fitToSection: true,
-            fitToSectionDelay: 1000,
-            scrollBar: false,
-            easing: 'easeInOutCubic',
-            easingcss3: 'ease',
-            loopBottom: false,
-            loopTop: false,
-            loopHorizontal: true,
-            continuousVertical: false,
-            continuousHorizontal: false,
-            scrollHorizontally: false,
-            //interlockedSlides: false,
-            dragAndMove: false,
-            offsetSections: false,
-            resetSliders: false,
-            fadingEffect: false,
-            //normalScrollElements: '#element1, .element2',
-            scrollOverflow: false,
-            scrollOverflowReset: false,
-            scrollOverflowOptions: null,
-            touchSensitivity: 15,
-            normalScrollElementTouchThreshold: 5,
-            bigSectionsDestination: 'top',
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: false,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        //interlockedSlides: false,
+        dragAndMove: false,
+        offsetSections: false,
+        resetSliders: false,
+        fadingEffect: false,
+        //normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowReset: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: 'top',
 
-            //Accessibility
-            keyboardScrolling: true,
-            animateAnchor: true,
-            recordHistory: true,
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
 
-            //Design
-            controlArrows: true,
-            verticalCentered: true,
-            sectionsColor : ['#7b7b7b'],
-            paddingTop: '5em',
-            paddingBottom: '10px',
-            fixedElements: '#header, .footer',
-            responsiveWidth: 0,
-            responsiveHeight: 0,
-            responsiveSlides: false,
-            parallax: false,
-            parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        sectionsColor : ['#7b7b7b'],
+        paddingTop: '5em',
+        paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+        responsiveSlides: false,
+        parallax: false,
+        parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
 
-            //Custom selectors
-            sectionSelector: '.section',
-            slideSelector: '.slide',
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
 
-            lazyLoading: true,
+        lazyLoading: true,
 
-            //events
-            onLeave: function(index, nextIndex, direction){},
-            afterLoad: function(anchorLink, index){},
-            afterRender: function(){},
-            afterResize: function(){},
-            afterResponsive: function(isResponsive){},
-            afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-            onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-        });
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterResponsive: function(isResponsive){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
     });
 };
 
