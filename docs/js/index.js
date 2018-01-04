8be82a4762eef8f8caacc9c11f9945abf4f9cea2
pascalnegwer92 <pascal@negwer.de>
2018-01-04 20:14:15 +0100

    [TASK] Adds flipped images in facts data

--- docs/js/index.js
+++ docs/js/index.js
@@ -86,6 +86,218 @@ var fullpage = function () {
 };
 
 fullpage();
+(function () {
+
+    $("#card1").flip({
+        axis: 'y',
+        trigger: 'hover'
+    });
+    $("#card2").flip({
+        axis: 'y',
+        trigger: 'hover'
+    });
+    $("#card3").flip({
+        axis: 'y',
+        trigger: 'hover'
+    });
+}());
+
+var donutCharts = (function () {
+    function getConfig(isMale) {
+        return {
+            type: 'pie',
+                data: {
+            datasets: [{
+                backgroundColor: [
+                    isMale ? '#48A3BB' : '#f2a68c',
+                    '#c7c7c7'
+                ],
+                borderWidth: 1,
+                data: [
+                    0,
+                    100
+                ]
+            }]
+        },
+            options: {
+                cutoutPercentage: 50,
+                tooltips: {
+                    enabled: false
+                },
+                legend: {
+                    display: false
+                },
+            }
+        };
+    }
+
+    var womanPie = new Chart(document.getElementById('donut-chart-woman'),getConfig(0));
+    var manPie = new Chart(document.getElementById('donut-chart-man'),getConfig(1));
+
+    var changeWomanData = function(number) {
+        womanPie.data.datasets[0].data = [
+            number,
+            100 - number
+        ];
+        womanPie.update();
+    };
+
+    var changeManData = function(number) {
+        manPie.data.datasets[0].data = [
+            number,
+            100 - number
+        ];
+        manPie.update();
+    };
+
+    return {
+        changeWomanData: changeWomanData,
+        changeManData: changeManData
+    };
+}());
+
+function lineChart() {
+    var barChartData = {
+        labels: ['< 1', '1 - 3', '4 - 10', '11 - 20', '20 +'],
+        datasets: [{
+            //label: 'Dataset 1',
+            backgroundColor: '#48A3BB',
+            hoverBackgroundColor: "#f2a68c",
+            borderWidth: 1,
+            data: [
+                40,
+                22,
+                22,
+                12,
+                4
+            ]
+        }]
+
+    };
+
+    var ctx = document.getElementById('line-chart');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: barChartData,
+        options: {
+            responsive: true,
+            tooltips: {
+                enabled: false
+            },
+            layout: {
+                padding: {
+                    left: 0,
+                    right: 50,
+                    top: 100,
+                    bottom: 0
+                }
+            },
+            maintainAspectRatio: false,
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    ticks: {
+                        fontColor: '#FFFFFF'
+                    },
+                    gridLines: {
+                        lineWidth: 3,
+                        display: false,
+                        color: '#FFFFFF'
+                    },
+                    scaleLabel: {
+                        fontColor: '#FFFFFF',
+                        fontSize: '15',
+                        display: true,
+                        labelString: 'Berufserfahrung in Jahren'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        fontColor: '#FFFFFF'
+                    },
+                    gridLines: {
+                        lineWidth: 3,
+                        display: false,
+                        color: '#FFFFFF'
+                    }
+                }]
+            },
+            animation: {
+                onProgress: function () {
+                    var chartInstance = this.chart;
+                    var ctx = chartInstance.ctx;
+                    ctx.textAlign = "center";
+                    Chart.helpers.each(this.data.datasets.forEach(function (dataset, i) {
+                        var meta = chartInstance.controller.getDatasetMeta(i);
+                        Chart.helpers.each(meta.data.forEach(function (bar, index) {
+                            ctx.fillText(dataset.data[index] + ' %', bar._model.x, (chartInstance.height + bar._model.y) * 0.5 - 20);
+                        }),this);
+                    }),this);
+                }
+            },
+            onClick: function (ev) {
+                var point = this.getElementAtEvent(ev);
+                var womansPercentage = 0;
+                var malePercentage = 0;
+                var text;
+                switch (point[0]._index) {
+                    case 0:
+                        womansPercentage = 36;
+                        malePercentage = 41;
+                        text = 'bis zu einem Jahr';
+                        break;
+                    case 1:
+                        womansPercentage = 26;
+                        malePercentage = 21;
+                        text = 'zwischen einem und drei Jahren';
+                        break;
+                    case 2:
+                        womansPercentage = 24;
+                        malePercentage = 22;
+                        text = 'zwischen vier und zehn Jahren';
+                        break;
+                    case 3:
+                        womansPercentage = 11;
+                        malePercentage = 12;
+                        text = 'zwischen elf und zwanzig Jahren';
+                        break;
+                    case 4:
+                        womansPercentage = 3;
+                        malePercentage = 4;
+                        text = 'mehr als zwanzig Jahre';
+                        break;
+                }
+                donutCharts.changeWomanData(womansPercentage);
+                donutCharts.changeManData(malePercentage);
+                countUp(document.getElementById('woman-percentage'), womansPercentage);
+                countUp(document.getElementById('male-percentage'), malePercentage);
+                var elements = document.getElementsByClassName('workexperience-years');
+
+                [].forEach.call(elements, function (element) {
+                    console.log(element);
+                    element.innerText = text;
+                });
+            },
+            hover: {
+                onHover: function(e) {
+                    var point = this.getElementAtEvent(e);
+                    if (point.length) {
+                        e.target.style.cursor = 'pointer';
+                    } else {
+                        e.target.style.cursor = 'default';
+                    }
+                }
+            }
+        }
+    });
+
+}
+
+lineChart();
 function getPersonalityData() {
     return [
         {
@@ -567,200 +779,3 @@ var hoverEvents = function() {
 document.addEventListener("DOMContentLoaded", function(ev) {
     hoverEvents();
 });
-
-var donutCharts = (function () {
-    function getConfig(isMale) {
-        return {
-            type: 'pie',
-                data: {
-            datasets: [{
-                backgroundColor: [
-                    isMale ? '#48A3BB' : '#f2a68c',
-                    '#c7c7c7'
-                ],
-                borderWidth: 1,
-                data: [
-                    0,
-                    100
-                ]
-            }]
-        },
-            options: {
-                cutoutPercentage: 50,
-                tooltips: {
-                    enabled: false
-                },
-                legend: {
-                    display: false
-                },
-            }
-        };
-    }
-
-    var womanPie = new Chart(document.getElementById('donut-chart-woman'),getConfig(0));
-    var manPie = new Chart(document.getElementById('donut-chart-man'),getConfig(1));
-
-    var changeWomanData = function(number) {
-        womanPie.data.datasets[0].data = [
-            number,
-            100 - number
-        ];
-        womanPie.update();
-    };
-
-    var changeManData = function(number) {
-        manPie.data.datasets[0].data = [
-            number,
-            100 - number
-        ];
-        manPie.update();
-    };
-
-    return {
-        changeWomanData: changeWomanData,
-        changeManData: changeManData
-    };
-}());
-
-function lineChart() {
-    var barChartData = {
-        labels: ['< 1', '1 - 3', '4 - 10', '11 - 20', '20 +'],
-        datasets: [{
-            //label: 'Dataset 1',
-            backgroundColor: '#48A3BB',
-            hoverBackgroundColor: "#f2a68c",
-            borderWidth: 1,
-            data: [
-                40,
-                22,
-                22,
-                12,
-                4
-            ]
-        }]
-
-    };
-
-    var ctx = document.getElementById('line-chart');
-    var chart = new Chart(ctx, {
-        type: 'bar',
-        data: barChartData,
-        options: {
-            responsive: true,
-            tooltips: {
-                enabled: false
-            },
-            layout: {
-                padding: {
-                    left: 0,
-                    right: 50,
-                    top: 100,
-                    bottom: 0
-                }
-            },
-            maintainAspectRatio: false,
-            legend: {
-                display: false
-            },
-            scales: {
-                xAxes: [{
-                    display: true,
-                    ticks: {
-                        fontColor: '#FFFFFF'
-                    },
-                    gridLines: {
-                        lineWidth: 3,
-                        display: false,
-                        color: '#FFFFFF'
-                    },
-                    scaleLabel: {
-                        fontColor: '#FFFFFF',
-                        fontSize: '15',
-                        display: true,
-                        labelString: 'Berufserfahrung in Jahren'
-                    }
-                }],
-                yAxes: [{
-                    display: true,
-                    ticks: {
-                        fontColor: '#FFFFFF'
-                    },
-                    gridLines: {
-                        lineWidth: 3,
-                        display: false,
-                        color: '#FFFFFF'
-                    }
-                }]
-            },
-            animation: {
-                onProgress: function () {
-                    var chartInstance = this.chart;
-                    var ctx = chartInstance.ctx;
-                    ctx.textAlign = "center";
-                    Chart.helpers.each(this.data.datasets.forEach(function (dataset, i) {
-                        var meta = chartInstance.controller.getDatasetMeta(i);
-                        Chart.helpers.each(meta.data.forEach(function (bar, index) {
-                            ctx.fillText(dataset.data[index] + ' %', bar._model.x, (chartInstance.height + bar._model.y) * 0.5 - 20);
-                        }),this);
-                    }),this);
-                }
-            },
-            onClick: function (ev) {
-                var point = this.getElementAtEvent(ev);
-                var womansPercentage = 0;
-                var malePercentage = 0;
-                var text;
-                switch (point[0]._index) {
-                    case 0:
-                        womansPercentage = 36;
-                        malePercentage = 41;
-                        text = 'bis zu einem Jahr';
-                        break;
-                    case 1:
-                        womansPercentage = 26;
-                        malePercentage = 21;
-                        text = 'zwischen einem und drei Jahren';
-                        break;
-                    case 2:
-                        womansPercentage = 24;
-                        malePercentage = 22;
-                        text = 'zwischen vier und zehn Jahren';
-                        break;
-                    case 3:
-                        womansPercentage = 11;
-                        malePercentage = 12;
-                        text = 'zwischen elf und zwanzig Jahren';
-                        break;
-                    case 4:
-                        womansPercentage = 3;
-                        malePercentage = 4;
-                        text = 'mehr als zwanzig Jahre';
-                        break;
-                }
-                donutCharts.changeWomanData(womansPercentage);
-                donutCharts.changeManData(malePercentage);
-                countUp(document.getElementById('woman-percentage'), womansPercentage);
-                countUp(document.getElementById('male-percentage'), malePercentage);
-                var elements = document.getElementsByClassName('workexperience-years');
-
-                [].forEach.call(elements, function (element) {
-                    console.log(element);
-                    element.innerText = text;
-                });
-            },
-            hover: {
-                onHover: function(e) {
-                    var point = this.getElementAtEvent(e);
-                    if (point.length) {
-                        e.target.style.cursor = 'pointer';
-                    } else {
-                        e.target.style.cursor = 'default';
-                    }
-                }
-            }
-        }
-    });
-
-}
-
-lineChart();
---
+++ src/js/facts/flip.js
@@ -0,0 +1,15 @@
+(function () {
+
+    $("#card1").flip({
+        axis: 'y',
+        trigger: 'hover'
+    });
+    $("#card2").flip({
+        axis: 'y',
+        trigger: 'hover'
+    });
+    $("#card3").flip({
+        axis: 'y',
+        trigger: 'hover'
+    });
+}());
