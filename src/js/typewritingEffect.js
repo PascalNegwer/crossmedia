7e05b06eeb58c7ddd0de7cf12875ddb6ca32ce29
Linda <linda.jansen@gmx.de>
2018-01-23 18:03:35 +0100

    Typingeffect auf Startseite eingefügt, finals Styling und Typingeffect im Impressum

--- src/js/typewritingEffect.js
+++ src/js/typewritingEffect.js
@@ -1,60 +1,41 @@
-/**
- * Created by jansenli on 03.01.2018.
- */
 
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
+document.addEventListener('DOMContentLoaded',function(event){
+    // array with texts to type in typewriter
+    var dataText = ['typing1', 'typing2', 'typing3'];
+    var elemIndex = 0;
+    var text;
 
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+    // type one text in the typwriter
+    // keeps calling itself until the text is finished
+    function typeWriter(text, i, fnCallback) {
+        // chekc if text isn't finished yet
+        if (i < (text.length)) {
+            // add next character to h1
+            document.getElementById(dataText[elemIndex]).innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
 
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-    }
-
-    setTimeout(function() {
-        that.tick();
-    }, delta);
-};
-
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-            new TxtType(elements[i], JSON.parse(toRotate), period);
+            // wait for a while and call this function again for next character
+            setTimeout(function() {
+                typeWriter(text, i + 1, fnCallback);
+            }, 150);
+        }
+        // text finished, call callback if there is a callback function
+        else if (typeof fnCallback === 'function') {
+            // call callback after timeout
+            elemIndex++;
+            setTimeout(fnCallback(), 700);
         }
     }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid }";
-    document.body.appendChild(css);
-};
+
+    var nextElement = function() {
+        //console.log(elemIndex);
+        if (elemIndex >= dataText.length) {
+            elemIndex = 0;
+        }
+        text = document.getElementById(dataText[elemIndex]).innerText;
+        typeWriter(text, 0, nextElement);
+    };
+
+    text = document.getElementById(dataText[elemIndex]).innerText;
+
+    typeWriter(text, 0, nextElement);
+});
